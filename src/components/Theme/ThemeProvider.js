598c20a6488b8a91986abf1a48d320c76f2a3c5b
Abhinav <shrivastavaabhinav47@gmail.com>
2023-12-23 12:35:34 +0530

    JsDoc generated for ThemeProvider

--- src/components/Theme/ThemeProvider.js
+++ src/components/Theme/ThemeProvider.js
@@ -3,12 +3,31 @@ import React, { createContext, useContext, useEffect, useState } from 'react';
 import { Appearance } from 'react-native';
 import {lightTheme as defaultLightTheme, darkTheme as defaultDarkTheme } from '../../utils/themes'
 
+/**
+ * @typedef {Object} ThemeContextValue
+ * @property {string} theme - The current theme ('light' or 'dark').
+ * @property {Function} toggleTheme - Function to toggle between light and dark themes.
+ * @property {CustomTheme} currentTheme - The current merged theme object.
+ */
+
 const ThemeContext = createContext();
 
+/**
+ * Custom hook to access the theme context values.
+ * @returns {ThemeContextValue} Theme context values.
+ */
 export const useTheme = () => {
   return useContext(ThemeContext);
 };
 
+/**
+ * Component to provide theme context to the application.
+ * @param {Object} props - Component props.
+ * @param {React.ReactNode} props.children - Child components.
+ * @param {CustomTheme} props.customLightTheme - Customized properties for the light theme.
+ * @param {CustomTheme} props.customDarkTheme - Customized properties for the dark theme.
+ * @returns {React.JSX} ThemeProvider component.
+ */
 export const ThemeProvider = ({ children, customLightTheme, customDarkTheme }) => {
   const [theme, setTheme] = useState(Appearance.getColorScheme());
 
@@ -20,6 +39,9 @@ export const ThemeProvider = ({ children, customLightTheme, customDarkTheme }) =
     return () => subscription.remove();
   }, []);
 
+  /**
+   * Toggle between light and dark themes.
+   */
   const toggleTheme = () => {
     setTheme(theme === 'dark' ? 'light' : 'dark');
   };
--- src/utils/themes.js
+++ src/utils/themes.js
@@ -1,4 +1,28 @@
-// themes.js
+/**
+ * Light theme object containing properties for the light theme.
+ * @typedef {Object} LightTheme
+ * @property {string} primaryColor - The primary color for the light theme.
+ * @property {string} secondaryColor - The secondary color for the light theme.
+ * @property {string} textColor - Common text color for light theme.
+ * @property {string} backgroundColor - Common background color for light theme.
+ * 
+ */
+
+
+/**
+ * Dark theme object containing properties for the dark theme.
+ * @typedef {Object} DarkTheme
+ * @property {string} primaryColor - The primary color for the dark theme.
+ * @property {string} secondaryColor - The secondary color for the dark theme.
+ * @property {string} textColor - Common text color for dark theme.
+ * @property {string} backgroundColor - Common background color for dark theme.
+ * Other properties...
+ */
+
+/**
+ * Light theme object containing properties for the light theme.
+ * @type {LightTheme}
+ */
 export const lightTheme = {
     primaryColor: '#3498db',
     secondaryColor: '#2ecc71',
@@ -7,6 +31,10 @@ export const lightTheme = {
     // Other light theme properties
   };
   
+/**
+ * Dark theme object containing properties for the dark theme.
+ * @type {DarkTheme}
+ */
   export const darkTheme = {
     primaryColor: '#9b59b6',
     secondaryColor: '#e74c3c',
